27fcbe87eff93f227706cd490aa8fcbd6f6b7488
Nacho Martín <nacho@limenius.com>
2017-06-11 20:05:19 +0200

    Trying to reproduce bug

---
+++ client/js/recipes/startup/AnotherComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default (initialProps) => {
+    return (
+        <h1>
+            Hi, I am a separate component
+        </h1>
+    )
+}
--- client/js/recipes/startup/clientRegistration.js
+++ client/js/recipes/startup/clientRegistration.js
@@ -1,5 +1,7 @@
 import ReactOnRails from 'react-on-rails'
 import RecipesApp from './RecipesAppClient'
+import AnotherComponent from './AnotherComponent'
 
 ReactOnRails.register({ RecipesApp })
+ReactOnRails.register({ AnotherComponent })
 
